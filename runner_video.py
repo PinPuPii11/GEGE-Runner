f1eec354ef72bb4bc4d7eb0b672fa76682f87704
Hanazono <intuch68@gmail.com>
2024-02-22 03:44:35 +0700

    implement camera

--- runner_video.py
+++ runner_video.py
@@ -1,26 +1,52 @@
 import pygame
 from sys import exit
 from random import randint, choice
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
+parameters = aruco.DetectorParameters()
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+
+player_walk_pic_1 = "/Users/ant/very new junk program/GEGE-Runner/graphics/Player/player_walk_1.png"
+player_walk_pic_2 = "/Users/ant/very new junk program/GEGE-Runner/graphics/Player/player_walk_2.png"
+player_jump_pic = "/Users/ant/very new junk program/GEGE-Runner/graphics/Player/jump.png"
+player_stand_pic = "/Users/ant/very new junk program/GEGE-Runner/graphics/Player/player_stand.png"
+jumpmp3 = "/Users/ant/very new junk program/GEGE-Runner/audio/jump.mp3"
+fly_pic_1 = "/Users/ant/very new junk program/GEGE-Runner/graphics/Fly/Fly1.png"
+fly_pic_2 = "/Users/ant/very new junk program/GEGE-Runner/graphics/Fly/Fly2.png"
+snail_pic_1 = "/Users/ant/very new junk program/GEGE-Runner/graphics/snail/snail1.png"
+snail_pic_2 = "/Users/ant/very new junk program/GEGE-Runner/graphics/snail/snail2.png"
+font_game = "/Users/ant/very new junk program/GEGE-Runner/font/Pixeltype.ttf"
+music_game = "/Users/ant/very new junk program/GEGE-Runner/audio/music.wav"
+ground_pic = "/Users/ant/very new junk program/GEGE-Runner/graphics/ground.png"
+sky_pic = "/Users/ant/very new junk program/GEGE-Runner/graphics/Sky.png"
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		super().__init__()
-		player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-		player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+		player_walk_1 = pygame.image.load(player_walk_pic_1).convert_alpha()
+		player_walk_2 = pygame.image.load(player_walk_pic_2).convert_alpha()
 		self.player_walk = [player_walk_1,player_walk_2]
 		self.player_index = 0
-		self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+		self.player_jump = pygame.image.load(player_jump_pic).convert_alpha()
 
 		self.image = self.player_walk[self.player_index]
 		self.rect = self.image.get_rect(midbottom = (80,300))
 		self.gravity = 0
 
-		self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+		self.jump_sound = pygame.mixer.Sound(jumpmp3)
 		self.jump_sound.set_volume(0.5)
 
-	def player_input(self):
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+	def player_input(self,y,rj):
+		
+		# keys = pygame.key.get_pressed()
+		print(y,rj)
+		if y < 200 and rj == True and self.rect.bottom == 300:
 			self.gravity = -20
 			self.jump_sound.play()
 
@@ -38,8 +64,8 @@ class Player(pygame.sprite.Sprite):
 			if self.player_index >= len(self.player_walk):self.player_index = 0
 			self.image = self.player_walk[int(self.player_index)]
 
-	def update(self):
-		self.player_input()
+	def update(self,y,rj):
+		self.player_input(y,rj)
 		self.apply_gravity()
 		self.animation_state()
 
@@ -48,13 +74,13 @@ class Obstacle(pygame.sprite.Sprite):
 		super().__init__()
 		
 		if type == 'fly':
-			fly_1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
-			fly_2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+			fly_1 = pygame.image.load(fly_pic_1).convert_alpha()
+			fly_2 = pygame.image.load(fly_pic_2).convert_alpha()
 			self.frames = [fly_1,fly_2]
 			y_pos = 210
 		else:
-			snail_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-			snail_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+			snail_1 = pygame.image.load(snail_pic_1).convert_alpha()
+			snail_2 = pygame.image.load(snail_pic_2).convert_alpha()
 			self.frames = [snail_1,snail_2]
 			y_pos  = 300
 
@@ -123,11 +149,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('GEGE Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+test_font = pygame.font.Font(font_game, 50)
 game_active = False
 start_time = 0
 score = 0
-bg_music = pygame.mixer.Sound('audio/music.wav')
+bg_music = pygame.mixer.Sound(music_game)
 bg_music.play(loops = -1)
 
 #Groups
@@ -136,22 +162,22 @@ player.add(Player())
 
 obstacle_group = pygame.sprite.Group()
 
-sky_surface = pygame.image.load('graphics/Sky.png').convert()
-ground_surface = pygame.image.load('graphics/ground.png').convert()
+sky_surface = pygame.image.load(sky_pic).convert()
+ground_surface = pygame.image.load(ground_pic).convert()
 
 # score_surf = test_font.render('My game', False, (64,64,64))
 # score_rect = score_surf.get_rect(center = (400,50))
 
 # Snail 
-snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+snail_frame_1 = pygame.image.load(snail_pic_1).convert_alpha()
+snail_frame_2 = pygame.image.load(snail_pic_2).convert_alpha()
 snail_frames = [snail_frame_1, snail_frame_2]
 snail_frame_index = 0
 snail_surf = snail_frames[snail_frame_index]
 
 # Fly
-fly_frame1 = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
-fly_frame2 = pygame.image.load('graphics/fly/fly2.png').convert_alpha()
+fly_frame1 = pygame.image.load(fly_pic_1).convert_alpha()
+fly_frame2 = pygame.image.load(fly_pic_2).convert_alpha()
 fly_frames = [fly_frame1, fly_frame2]
 fly_frame_index = 0
 fly_surf = fly_frames[fly_frame_index]
@@ -159,18 +185,18 @@ fly_surf = fly_frames[fly_frame_index]
 obstacle_rect_list = []
 
 
-player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+player_walk_1 = pygame.image.load(player_walk_pic_1).convert_alpha()
+player_walk_2 = pygame.image.load(player_walk_pic_2).convert_alpha()
 player_walk = [player_walk_1,player_walk_2]
 player_index = 0
-player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+player_jump = pygame.image.load(player_jump_pic).convert_alpha()
 
 player_surf = player_walk[player_index]
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
 
 # Intro screen
-player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.image.load(player_stand_pic).convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand,0,2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
@@ -189,7 +215,8 @@ pygame.time.set_timer(snail_animation_timer,500)
 
 fly_animation_timer = pygame.USEREVENT + 3
 pygame.time.set_timer(fly_animation_timer,200)
-
+resetjump = True
+player_y = 99999
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -247,9 +274,33 @@ while True:
 		# if player_rect.bottom >= 300: player_rect.bottom = 300
 		# player_animation()
 		# screen.blit(player_surf,player_rect)
+		ret, frame = cap.read()
+		image = frame
+		gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+		detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+		_, thresholded = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+		corners, ids, rejectedImgPoints = detector.detectMarkers(thresholded)
+			# delta time
+		if ids is not None:
+       		 # Loop through each dextected marker
+			for i in range(len(ids)):
+            		# Extract the corners of the marker
+				marker_corners = corners[i][0].astype(np.int32)
+            		# Reshape the marker corners to match the expected format
+				marker_corners = marker_corners.reshape((-1, 1, 2)).astype(np.int32)
+				cv2.polylines(frame, [marker_corners] , True, (0, 255, 0), thickness = 2)
+				player_y  = (abs(marker_corners[0][0][1]))
+			cv2.putText(frame, str(marker_corners[0][0][1]), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+			
+		cv2.imshow('MediaPipe Pose', frame)    
+		if cv2.waitKey(10) & 0xFF == ord('q'):
+			break
+		if player_y < 200 :
+			resetjump = True
+		elif player_y >= 400 :
+			resetjump = False
 		player.draw(screen)
-		player.update()
-
+		player.update(player_y,resetjump)
 		obstacle_group.draw(screen)
 		obstacle_group.update()
 
